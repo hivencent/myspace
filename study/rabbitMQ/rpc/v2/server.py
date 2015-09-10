65fcb62fd155ebaa7c597cce5301338b26a990f8
hivencent <shadalang@gmail.com>
2015-09-10 15:22:55 +0800

    modify

--- study/rabbitMQ/rpc/v2/server.py
+++ study/rabbitMQ/rpc/v2/server.py
@@ -3,10 +3,10 @@ __author__ = 'jinlong'
 import time
 from rpyc import Service
 from rpyc.utils.server import ThreadedServer
-
 class TimeService(Service):
     def exposed_sum(self,a,b):
         print a,b
         return a+b
-s=ThreadedServer(TimeService,port=22,auto_register=False)
+
+s=ThreadedServer(TimeService,port=12233,auto_register=False)
 s.start()
