b7fa756a7d6e3a959dd93d39d23fedb95ced62dc
hivencent <shadalang@gmail.com>
2015-09-10 15:09:17 +0800

    modify

--- study/rabbitMQ/rpc/v2/clent.py
+++ study/rabbitMQ/rpc/v2/clent.py
@@ -2,6 +2,6 @@
 __author__ = 'jinlong'
 import rpyc
 
-c=rpyc.connect('localhost',12233)
+c=rpyc.connect('localhost',22)
 print c.root.sum(1,2)
 c.close()
--- study/rabbitMQ/rpc/v2/server.py
+++ study/rabbitMQ/rpc/v2/server.py
@@ -6,6 +6,7 @@ from rpyc.utils.server import ThreadedServer
 
 class TimeService(Service):
     def exposed_sum(self,a,b):
+        print a,b
         return a+b
-s=ThreadedServer(TimeService,port=12233,auto_register=False)
+s=ThreadedServer(TimeService,port=22,auto_register=False)
 s.start()
